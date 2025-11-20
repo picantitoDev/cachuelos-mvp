bfeec5f03d24ae2f43eecafec8484e6bb85ed49b
Piero Arturo <155034777+picantitoDev@users.noreply.github.com>
2025-11-20 18:50:35 -0500

    fix: prisma 7 client + vercel build

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,22 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
+  connectionString: process.env.DATABASE_URL!,
+});
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
+// Fix para Vercel + Hot reload local
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
-export default prisma
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prisma;
+
+export default prisma;
